0f87fd3e2549931cb7cc441a246ea8b3fbef85b8
Pawel <100613143+RosinskiPawel@users.noreply.github.com>
2023-03-30 13:29:33 +0200

    added a loop

--- 101-exam/TicTacToe.py
+++ 101-exam/TicTacToe.py
@@ -1,8 +1,8 @@
 import random
 
 matrix_master = [['0.0','0.1','0.2'], ['1.0','1.1','1.2'], ['2.0','2.1','2.2']]
-matrix = [[' ', ' ',' '], [' ', ' ', ' '], [' ', ' ', ' ']]
-pool = [0.0, 0.1, 0.2, 1.0, 1.1, 1.2, 2.0, 2.1, 2.2]
+# matrix = [[' ', ' ',' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+# pool = [0.0, 0.1, 0.2, 1.0, 1.1, 1.2, 2.0, 2.1, 2.2]
 winner = False
 
 def horizontal_win(a,b):
@@ -40,80 +40,67 @@ def diagonal_win(a, b):
             winner = True 
             return winner
 
-# while winner == False:
-     
-#     chosen_by_player = input("Please put the 'X' ")
-#     player = chosen_by_player.split('.')
-#     matrix[int(player[0])][int(player[1])] = 'X'
-#     pool.remove(float(chosen_by_player))
-#     print(f"{matrix[0]}\n{matrix[1]}\n{matrix[2]}")
-#     if (horizontal_win() == True or vertical_win() == True or diagonal_win() == True):
-#         print("The End")
-#         break
-#     if len(pool)==0:
-#         print("The End. Draw!")
-#         break
-    
-#     print(f"\nNow it's my turn\n")
-#     random_from_pool = random.choice(pool)
-#     pool.remove(float(random_from_pool))
-#     chosen_by_comp = str(random_from_pool).split('.')
-#     matrix[int(chosen_by_comp[0])][int(chosen_by_comp[1])] = 'O'
-#     print(f"{matrix[0]}\n{matrix[1]}\n{matrix[2]}\n")
-#     if (horizontal_win() == True or vertical_win() == True or diagonal_win() == True):
-#         print("The End")
-#         break
-    
-players_to_choose = input(f"Please choose the players:\n Human vs. Computer = 1\n Player1 vs. Player2 = 2\n\n\t")
-print(f"This is the matrix we will use to play.\n\n{matrix_master[0]}\n{matrix_master[1]}\n{matrix_master[2]}\n")
-if (players_to_choose == '1'):
-    while winner == False:
-    
-        chosen_by_player = input("Please put the 'X': ")
-        player = chosen_by_player.split('.')
-        matrix[int(player[0])][int(player[1])] = 'X'
-        pool.remove(float(chosen_by_player))
-        print(f"{matrix[0]}\n{matrix[1]}\n{matrix[2]}")
-        if (horizontal_win('Player', 'Computer') == True or vertical_win('Player', 'Computer') == True or diagonal_win('Player', 'Computer') == True):
-            print("The End")
-            break
-        if len(pool)==0:
-            print("The End. Draw!")
-            break
+while True:
+    pool = [0.0, 0.1, 0.2, 1.0, 1.1, 1.2, 2.0, 2.1, 2.2]  
+    matrix = [[' ', ' ',' '], [' ', ' ', ' '], [' ', ' ', ' ']]  
+    players_to_choose = input(f"Please choose the players:\n Human vs. Computer = 1\n Player1 vs. Player2 = 2\n\n\t")
+    print(f"This is the matrix we will use to play.\n\n{matrix_master[0]}\n{matrix_master[1]}\n{matrix_master[2]}\n")
+    if (players_to_choose == '1'):
+        while winner == False:
         
-        print(f"\nNow it's my turn\n")
-        random_from_pool = random.choice(pool)
-        pool.remove(float(random_from_pool))
-        chosen_by_comp = str(random_from_pool).split('.')
-        matrix[int(chosen_by_comp[0])][int(chosen_by_comp[1])] = 'O'
-        print(f"{matrix[0]}\n{matrix[1]}\n{matrix[2]}\n")
-        if (horizontal_win('Player', 'Computer') == True or vertical_win('Player', 'Computer') == True or diagonal_win('Player', 'Computer') == True):
-            print("The End")
-            break
-else:
-    while winner == False:
-    
-        chosen_by_player = input("Player 1: Please put the 'X': ")
-        player = chosen_by_player.split('.')
-        matrix[int(player[0])][int(player[1])] = 'X'
-        pool.remove(float(chosen_by_player))
-        print(f"{matrix[0]}\n{matrix[1]}\n{matrix[2]}")
-        if (horizontal_win('Player 1', 'Player 2') == True or vertical_win('Player 1', 'Player 2') == True or diagonal_win('Player 1', 'Player 2') == True):
-            print("The End")
-            break
-        if len(pool)==0:
-            print("The End. Draw!")
-            break
+            chosen_by_player = input("Please put the 'X': ")
+            player = chosen_by_player.split('.')
+            matrix[int(player[0])][int(player[1])] = 'X'
+            pool.remove(float(chosen_by_player))
+            print(f"{matrix[0]}\n{matrix[1]}\n{matrix[2]}")
+            if (horizontal_win('Player', 'Computer') == True or vertical_win('Player', 'Computer') == True or diagonal_win('Player', 'Computer') == True):
+                print("The End")
+                break
+            if len(pool)==0:
+                print("The End. Draw!")
+                break
+            
+            print(f"\nNow it's my turn\n")
+            random_from_pool = random.choice(pool)
+            pool.remove(float(random_from_pool))
+            chosen_by_comp = str(random_from_pool).split('.')
+            matrix[int(chosen_by_comp[0])][int(chosen_by_comp[1])] = 'O'
+            print(f"{matrix[0]}\n{matrix[1]}\n{matrix[2]}\n")
+            if (horizontal_win('Player', 'Computer') == True or vertical_win('Player', 'Computer') == True or diagonal_win('Player', 'Computer') == True):
+                print("The End")
+                break
+    else:
+        while winner == False:
         
-        chosen_by_player2 = input("Player 2: Please put the 'O': ")
-        player2 = chosen_by_player2.split('.')
-        matrix[int(player2[0])][int(player2[1])] = 'O'
-        pool.remove(float(chosen_by_player2))
-        print(f"{matrix[0]}\n{matrix[1]}\n{matrix[2]}\n")
-        if (horizontal_win('Player 1', 'Player 2') == True or vertical_win('Player 1', 'Player 2') == True or diagonal_win('Player 1', 'Player 2') == True):
-            print("The End")
-            break
-       
+            chosen_by_player = input("Player 1: Please put the 'X': ")
+            player = chosen_by_player.split('.')
+            matrix[int(player[0])][int(player[1])] = 'X'
+            pool.remove(float(chosen_by_player))
+            print(f"{matrix[0]}\n{matrix[1]}\n{matrix[2]}")
+            if (horizontal_win('Player 1', 'Player 2') == True or vertical_win('Player 1', 'Player 2') == True or diagonal_win('Player 1', 'Player 2') == True):
+                print("The End")
+                break
+            if len(pool)==0:
+                print("The End. Draw!")
+                break
+            
+            chosen_by_player2 = input("Player 2: Please put the 'O': ")
+            player2 = chosen_by_player2.split('.')
+            matrix[int(player2[0])][int(player2[1])] = 'O'
+            pool.remove(float(chosen_by_player2))
+            print(f"{matrix[0]}\n{matrix[1]}\n{matrix[2]}\n")
+            if (horizontal_win('Player 1', 'Player 2') == True or vertical_win('Player 1', 'Player 2') == True or diagonal_win('Player 1', 'Player 2') == True):
+                print("The End")
+                break
+    
+    question = input("Wanna play again? (y/n):  ").lower()
+    if (question == 'y'):
+        True
+    else:
+        print("Thank you!")
+        break
+        
+        
         
 
     
